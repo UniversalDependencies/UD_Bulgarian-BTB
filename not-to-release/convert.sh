1dc9b41234205156b8f606edefa2df4e3492fe36
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-18 16:44:23 +0100

    better reconstruction of the original texts with goeswith
    See also https://github.com/UniversalDependencies/docs/issues/424

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for a in train dev test; do
-    ./fix-sent_id.pl bg-ud-$a.conllu | udapy -s ud.bg.RemoveDotAfterAbbr ud.Convert1to2 skip=nmod > ../bg-ud-$a.conllu
+    ./fix-sent_id.pl bg-ud-$a.conllu | udapy -s ud.bg.RemoveDotAfterAbbr ud.GoeswithFromText keep_lemma=1 ud.Convert1to2 skip=nmod > ../bg-ud-$a.conllu
 done
 
 cat ../bg-ud-{train,dev,test}.conllu | udapy -HAMC ud.MarkBugs > bugs.html
